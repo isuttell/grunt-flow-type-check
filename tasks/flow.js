4bb9c9003531e078808d681e3111789c0475fc85
Isaac Suttell <isaac@isaacsuttell.com>
2014-11-22 12:11:28 -0800

    initial commit

---
+++ tasks/flow.js
@@ -0,0 +1,47 @@
+/*
+ * grunt-flow
+ * https://github.com/isuttell/grunt-flow
+ *
+ * Copyright (c) 2014 Isaac Suttell
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  function run(args, done) {
+    var child = grunt.util.spawn({
+      cmd: args.shift(),
+      args: args,
+      opts: { stdio :'inherit' }
+    }, function (err, result, code) {
+      if (code === 127) {
+        grunt.warn(
+          'You need to have flow installed ' +
+          'and in your system PATH for this task to work. ' +
+          'More info: https://github.com/isuttell/flow'
+        );
+      }
+
+      // Fail if we find errors
+      if(code === 2) {
+        return done(false);
+      }
+
+      done();
+    });
+  }
+
+  grunt.registerMultiTask('flow', 'Flow type checking', function() {
+    var options = this.options({
+      configFile: '.'
+    });
+    var callback = this.async();
+
+    run(['flow', 'check', options.configFile], function(err){
+      callback(err);
+    });
+  });
+
+};

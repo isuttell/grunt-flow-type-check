1e04e05f1d022ce73e96b8cf5473e2d926e53417
Isaac Suttell <isaac@isaacsuttell.com>
2014-11-23 09:42:23 -0800

    refactor code so itis more DRY

--- tasks/flow.js
+++ tasks/flow.js
@@ -10,9 +10,11 @@
 
 module.exports = function(grunt) {
 
+  // Flow control library
   var flow = require('./lib/flow').init(grunt);
 
   grunt.registerMultiTask('flow', 'Facebook\'s Flow static type checking', function() {
+    // Default options
     var options = this.options({
       configFile: '.',
       json: false,
@@ -23,30 +25,39 @@ module.exports = function(grunt) {
     var callback = this.async();
 
     // Default args and process options
-    var args = [options.configFile];
+    var args = [];
     var opts = {
       stdio: 'inherit'
     };
 
+    // Figure out what command to run
+    var commands = ['start', 'status', 'stop', 'check'];
+    if (commands.indexOf(this.args[0]) > -1) {
+      args.push(this.args);
+    } else {
+      // Default to a basic full check
+      args.push('check');
+    }
+
+    // Where is `.flowconfig`
+    args.push(options.configFile);
+
+    // Enable json output - mostly for testing
     if (options.json) {
-      // Enable json output - mostly for testing
       args.push('--json');
       delete opts.stdio;
     }
 
-    // Watch tasks
-    if (options.background === true) {
-      var availableArgs = ['start', 'status', 'stop'];
-      if (availableArgs.indexOf(this.args[0]) > -1) {
-        args.push(this.args);
-        args = [this.args, options.configFile];
-        flow.run(args, opts, function(err, output) {
-          if (err) {
-            callback(err);
-          }
-        });
+    // Run it
+    flow.run(args, opts, function(err, output) {
+      if (output) {
+        grunt.log.writeln(JSON.stringify(output));
       }
+      callback(err);
+    });
 
+    // Stop the server
+    if (options.background === true) {
       // When we catch CTRL+C kill the flow server
       process.on('SIGINT', function() {
         args.push('stop');
@@ -54,18 +65,8 @@ module.exports = function(grunt) {
           process.kill();
         });
       });
-
-    } else {
-
-      // By default we run a full check
-      args = ['check', options.configFile];
-      flow.run(args, opts, function(err, output) {
-        if (output) {
-          grunt.log.writeln(JSON.stringify(output));
-        }
-        callback(err);
-      });
     }
+
   });
 
 };
